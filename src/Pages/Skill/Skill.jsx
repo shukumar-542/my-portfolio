8f52749da283e193c727eb0e96775ac7c71c9e37
shukumar-542 <shukumar542@gmail.com>
2024-05-05 14:58:26 +0600

    Enhancement my skills sections

--- src/Pages/Skill/Skill.jsx
+++ src/Pages/Skill/Skill.jsx
@@ -1,86 +1,116 @@
-import Marquee from "react-fast-marquee";
-import img1 from '../../assets/skills/bootstrap.png'
-import img2 from '../../assets/skills/css.png'
-import img3 from '../../assets/skills/expressjs.png'
-import img4 from '../../assets/skills/figma.png'
-import img5 from '../../assets/skills/firebase.png'
-import img6 from '../../assets/skills/git.png'
-import img7 from '../../assets/skills/github-icon.png'
-import img8 from '../../assets/skills/html.png'
-import img9 from '../../assets/skills/js.png'
-import img10 from '../../assets/skills/jwt.png'
-import img11 from '../../assets/skills/mongo-db.png'
-import img12 from '../../assets/skills/nextjs.png'
-import img13 from '../../assets/skills/node.png'
-import img14 from '../../assets/skills/react.png'
-import img15 from '../../assets/skills/redux.png'
-import img16 from '../../assets/skills/Rest-API-1.png'
+
+import { SiNextdotjs, SiRedux,SiTypescript,SiTailwindcss,SiExpress ,SiFigma,SiReact ,SiJavascript ,SiFirebase ,SiNodedotjs     } from "react-icons/si";
+import { DiMongodb } from "react-icons/di";
+import { FaGithub } from "react-icons/fa";
 
 
 const Skill = () => {
     return (
-        <div className="my-container  bg-[#252734]">
-            <h1 className="text-white text-3xl lg:text-4xl font-bold text-center py-5">My Skills</h1>
-            <div className='flex items-center '>
-                <Marquee className="h-32">
-                    <div className='h-[85px]   w-[150px] mx-5 bg-slate-600/30 text-center backdrop-blur-sm rounded-2xl '>
-                        <img src={img1} className='h-[85px] mx-auto py-5' alt="" />
-                    </div>
-                    <div className='h-[80px]  w-[150px] mx-5 bg-slate-600/30 text-center backdrop-blur-sm rounded-2xl '>
-                        <img src={img2} className='h-[80px] mx-auto py-5' alt="" />
-                    </div>
-                    <div className='h-[80px]  w-[150px] mx-5 bg-slate-600/30  text-center backdrop-blur-sm rounded-2xl '>
-                        <img src={img3} className='h-[80px] mx-auto py-5' alt="" />
-                    </div>
-                    <div className='h-[80px]  w-[150px] mx-5 bg-slate-600/30  text-center backdrop-blur-sm rounded-2xl '>
-                        <img src={img4} className='h-[80px] mx-auto py-5' alt="" />
-                    </div>
-                    <div className='h-[80px]  w-[150px] mx-5 bg-slate-600/30  text-center backdrop-blur-sm rounded-2xl '>
-                        <img src={img5} className='h-[80px] mx-auto py-5' alt="" />
-                    </div>
-                    <div className='h-[80px]  w-[150px] mx-5 bg-slate-600/30  text-center backdrop-blur-sm rounded-2xl '>
-                        <img src={img6} className='h-[80px] mx-auto py-5' alt="" />
-                    </div>
-                    <div className='h-[80px]  w-[150px] mx-5 bg-slate-600/30  text-center backdrop-blur-sm rounded-2xl '>
-                        <img src={img7} className='h-[80px] mx-auto py-5' alt="" />
-                    </div>
-                    <div className='h-[80px]  w-[150px] mx-5 bg-slate-600/30  text-center backdrop-blur-sm rounded-2xl '>
-                        <img src={img8} className='h-[80px] mx-auto py-5' alt="" />
-                    </div>
-                    <div className='h-[80px]  w-[150px] mx-5 bg-slate-600/30  text-center backdrop-blur-sm rounded-2xl '>
-                        <img src={img9} className='h-[80px] mx-auto py-5' alt="" />
-                    </div>
-
-                    <div className='h-[80px]  w-[150px] mx-5 bg-slate-600/30  text-center backdrop-blur-sm rounded-2xl '>
-                        <img src={img10} className='h-[80px] mx-auto py-5' alt="" />
-                    </div>
-
-                    <div className='h-[80px]  w-[150px] mx-5 bg-slate-600/30  text-center backdrop-blur-sm rounded-2xl '>
-                        <img src={img11} className='h-[80px] mx-auto py-5' alt="" />
-                    </div>
-
-                    <div className='h-[80px]  w-[150px] mx-5 bg-slate-600/30  text-center backdrop-blur-sm rounded-2xl '>
-                        <img src={img12} className='h-[80px] mx-auto py-5' alt="" />
-                    </div>
-
-                    <div className='h-[80px]  w-[150px] mx-5 bg-slate-600/30  text-center backdrop-blur-sm rounded-2xl '>
-                        <img src={img13} className='h-[80px] mx-auto py-5' alt="" />
-                    </div>
-
-                    <div className='h-[80px]  w-[150px] mx-5 bg-slate-600/30  text-center backdrop-blur-sm rounded-2xl '>
-                        <img src={img14} className='h-[80px] mx-auto py-5' alt="" />
-                    </div>
-
-                    <div className='h-[80px]  w-[150px] mx-5 bg-slate-600/30  text-center backdrop-blur-sm rounded-2xl '>
-                        <img src={img15} className='h-[80px] mx-auto py-5' alt="" />
-                    </div>
-
-                    <div className='h-[80px]  w-[150px] mx-5 bg-slate-600/30  text-center backdrop-blur-sm rounded-2xl '>
-                        <img src={img16} className='h-[80px] mx-auto py-5' alt="" />
-                    </div>
-                </Marquee>
+        <div className="my-container   bg-[#0F0715]">
+            <div className="text-center">
+                <h1 className="bg-gradient-to-r from-[#8A54F7] via-[#BFA2FB] to-[#ECE2FE] inline-block text-transparent bg-clip-text text-3xl lg:text-4xl font-bold  py-5 text-center ">My Skills</h1>
             </div>
-            
+            <div className='grid grid-cols-1 md:grid-cols-6 gap-5 my-container'>
+                <div className=' flex items-center justify-center  py-5 bg-[#1e112c] text-center  rounded-md hover:bg-[#2A1454] transition-all duration-400 '>
+
+                    <div>
+                        <SiNextdotjs size={80} className='text-gray-200' />
+                        <p className='text-[#8750f7] font-semibold text-xl mt-2'>Next js</p>
+                    </div>
+
+                </div>
+
+                <div className=' flex items-center justify-center  py-5 bg-[#1e112c] text-center  rounded-md hover:bg-[#2A1454] transition-all duration-400 '>
+
+                    <div>
+                        <SiRedux size={80} className='text-[#7248B6]' />
+                        <p className='text-[#8750f7] font-semibold text-xl mt-2'>Redux</p>
+                    </div>
+
+                </div>
+                <div className=' flex items-center justify-center  py-5 bg-[#1e112c] text-center  rounded-md hover:bg-[#2A1454] transition-all duration-400 '>
+
+                    <div>
+                        <SiTypescript  size={80} className='text-[#0176C6]' />
+                        <p className='text-[#8750f7] font-semibold text-xl mt-2'>TypeScript</p>
+                    </div>
+
+                </div>
+                <div className=' flex items-center justify-center  py-5 bg-[#1e112c] text-center  rounded-md hover:bg-[#2A1454] transition-all duration-400 '>
+
+                    <div>
+                        <SiReact   size={80} className='text-[#00C6F7]' />
+                        <p className='text-[#8750f7] font-semibold text-xl mt-2'>React</p>
+                    </div>
+
+                </div>
+                <div className=' flex items-center justify-center  py-5 bg-[#1e112c] text-center  rounded-md hover:bg-[#2A1454] transition-all duration-400 '>
+
+                    <div>
+                        <SiJavascript    size={80} className='text-[#ECDA1D]' />
+                        <p className='text-[#8750f7] font-semibold text-xl mt-2'>Javascript</p>
+                    </div>
+
+                </div>
+                <div className=' flex items-center justify-center  py-5 bg-[#1e112c] text-center  rounded-md hover:bg-[#2A1454] transition-all duration-400 '>
+
+                    <div>
+                        <SiTailwindcss  size={80} className='text-[#06B0CD]' />
+                        <p className='text-[#8750f7] font-semibold text-xl mt-2'>Tailwind Css</p>
+                    </div>
+
+                </div>
+                <div className=' flex items-center justify-center  py-5 bg-[#1e112c] text-center  rounded-md hover:bg-[#2A1454] transition-all duration-400 '>
+
+                    <div>
+                        <SiExpress  size={80} className='text-gray-200' />
+                        <p className='text-[#8750f7] font-semibold text-xl mt-2'>Express</p>
+                    </div>
+
+                </div>
+                <div className=' flex items-center justify-center  py-5 bg-[#1e112c] text-center  rounded-md hover:bg-[#2A1454] transition-all duration-400 '>
+
+                    <div>
+                        <SiFigma  size={80} className='text-gray-200' />
+                        <p className='text-[#8750f7] font-semibold text-xl mt-2'>Figma</p>
+                    </div>
+
+                </div>
+                <div className=' flex items-center justify-center  py-5 bg-[#1e112c] text-center  rounded-md hover:bg-[#2A1454] transition-all duration-400 '>
+
+                    <div>
+                        <DiMongodb  size={80} className='text-[#569134]' />
+                        <p className='text-[#8750f7] font-semibold text-xl mt-2'>Mongodb</p>
+                    </div>
+
+                </div>
+                <div className=' flex items-center justify-center  py-5 bg-[#1e112c] text-center  rounded-md hover:bg-[#2A1454] transition-all duration-400 '>
+
+                    <div>
+                        <FaGithub  size={80} className='text-gray-200' />
+                        <p className='text-[#8750f7] font-semibold text-xl mt-2'>Github</p>
+                    </div>
+
+                </div>
+                <div className=' flex items-center justify-center  py-5 bg-[#1e112c] text-center  rounded-md hover:bg-[#2A1454] transition-all duration-400 '>
+
+                    <div>
+                        <SiFirebase   size={80} className='text-[#F3C43F]' />
+                        <p className='text-[#8750f7] font-semibold text-xl mt-2'>Firebase </p>
+                    </div>
+
+                </div>
+                <div className=' flex items-center justify-center  py-5 bg-[#1e112c] text-center  rounded-md hover:bg-[#2A1454] transition-all duration-400 '>
+
+                    <div>
+                        <SiNodedotjs    size={80} className='text-[#7CB701]' />
+                        <p className='text-[#8750f7] font-semibold text-xl mt-2'>Nodejs </p>
+                    </div>
+
+                </div>
+                
+            </div>
+
         </div>
     );
 };
